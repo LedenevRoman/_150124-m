5ecea9a472ca5e47a5e1fc1a1db3ee3b01c521ed
RomanLedenev <pommanbko@mail.ru>
2024-02-19 18:05:51 +0100

    commit1

---
+++ src/main/java/org/example/_2024_02_19/HelloWorld.java
@@ -0,0 +1,7 @@
+package org.example._2024_02_19;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello"); // вывод на консоль передаваемые аргументы
+    }
+}
