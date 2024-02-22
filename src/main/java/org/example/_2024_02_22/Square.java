cf7860dd1e9c6f5fe35f07969cdf392146b3a3be
RomanLedenev <pommanbko@mail.ru>
2024-02-22 11:55:00 +0100

    commit4

--- src/main/java/org/example/_2024_02_22/Square.java
+++ src/main/java/org/example/_2024_02_22/Square.java
@@ -1,6 +1,7 @@
 package org.example._2024_02_22;
 
 import org.example._2024_02_19.HelloWorld;
+import org.example._2024_02_20.ClassWork;
 
 import java.util.Scanner;
 
@@ -8,13 +9,20 @@ import java.util.Scanner;
 public class Square {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);  // создаём объект сканнера, для принятия параметров из консоли
-
+        ClassWork.main(null);            // вызов метода другого класса
         System.out.println("Enter side size: ");
         double side = sc.nextDouble();        // принимаем из консоли аргумент типа double
-/*        String string = scanner.nextLine(); */       // принимаем из консоли аргумент типа String
-/*        char ch = scanner.next().charAt(0);  */      // принимаем из консоли аргумент типа char
+/*        String string = sc.nextLine();        // принимаем из консоли аргумент типа String
+        char ch = sc.nextLine().charAt(0); */       // принимаем из консоли аргумент типа char
 
-        double area = side * side;
+        System.out.println("строка1 \n строка2 \n стро");
+
+        double area = calculateArea(side);
         System.out.println(area);
+    }
+
+    public static double calculateArea(double side) {
+        double area = side * side;
+        return area;
     }
 }
