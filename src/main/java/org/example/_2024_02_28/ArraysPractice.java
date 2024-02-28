15e6242048944ff6bd95d4f3613212ab0f8a818f
RomanLedenev <pommanbko@mail.ru>
2024-02-28 10:51:11 +0100

    commit8

--- src/main/java/org/example/_2024_02_28/ArraysPractice.java
+++ src/main/java/org/example/_2024_02_28/ArraysPractice.java
@@ -6,6 +6,7 @@ public class ArraysPractice {
     public static void main(String[] args) {
         int[] array1 = new int[10];
         int[] array2 = {0, 1, 2, 3, 4, 5, 6};
+        int[] array6 = {0, 1, 2, 3, 4, 5, 6};
         int[] array5 = {10, 0, 5, 9, 6, 4, 2, 3, 7, 8, 1};
         String[] strings = {"Bstring", "Astring", "bstring", "astring", "Русский"};
 
@@ -18,7 +19,7 @@ public class ArraysPractice {
         System.out.println(Arrays.toString(array3));
 
         int[] array4 = Arrays.copyOfRange(array2, 3, 6);  // метод copyOfRange копирует исходным массив с указанием
-                                                            // индексов по которым будет проводиться копирование исключая последний индекс
+                                                                     // индексов по которым будет проводиться копирование исключая последний индекс
         System.out.println(Arrays.toString(array4));
 
         Arrays.sort(array5);                                // сортируем массив по возрастанию
@@ -26,6 +27,20 @@ public class ArraysPractice {
 
         Arrays.sort(strings);
         System.out.println(Arrays.toString(strings));
+
+        int a = 100;
+        int b = 100;
+
+        System.out.println(array2);
+        System.out.println(array6);
+        System.out.println(array2 == array6);
+        System.out.println(Arrays.equals(array2, array6));    // метод equals сравнивает массивы по их содержимому
+
+        System.out.println(Arrays.binarySearch(array5, 6));   // метод binarySearch выводит индекс искомого элемента в отсортированном массиве
+
+        int[] array7 = new int[10];
+        Arrays.fill(array7, 8);                      // заполняет указанный массив значениями
+        System.out.println(Arrays.toString(array7));
     }
 
     public static void printArray(int[] array) {
